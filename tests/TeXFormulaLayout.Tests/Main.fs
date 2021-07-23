c433c9db261e8c2a04990c4df89ebeaeb5bc80d3
woclass <5158738+inkydragon@users.noreply.github.com>
2021-07-24 01:03:47 +0800

    [test] merge into main

--- tests/TeXFormulaLayout.Tests/Main.fs
+++ tests/TeXFormulaLayout.Tests/Main.fs
@@ -1,8 +1,22 @@
 namespace TeXFormulaLayout.Tests
+open Expecto
+
+
+module SayTests =
+    open TeXFormulaLayout
+
+    [<Tests>]
+    let tests = testList "Samples" [
+        testCase "Say nothing" <| fun _ ->
+            let subject = Say.nothing()
+            Expect.equal subject () "Not an absolute unit"
+        testCase "Say hello all" <| fun _ ->
+            let subject = Say.hello "all"
+            Expect.equal subject "Hello all" "You didn't say hello"
+    ]
+
 
 module ExpectoTemplate =
-
-    open Expecto
 
     [<EntryPoint>]
     let main argv =
