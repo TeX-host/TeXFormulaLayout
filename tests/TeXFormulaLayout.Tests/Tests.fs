a493f7c16c0ada3345f22b47ca234a23f95e29d7
woclass <5158738+inkydragon@users.noreply.github.com>
2021-07-24 01:01:32 +0800

    [test/LoadFont] split test

--- tests/TeXFormulaLayout.Tests/Tests.fs
+++ tests/TeXFormulaLayout.Tests/Tests.fs
@@ -4,8 +4,6 @@ open Expecto
 
 module SayTests =
     open TeXFormulaLayout
-    open TeXFormulaLayout.FontTypes
-    open TeXFormulaLayout.LoadFont
 
     [<Tests>]
     let tests =
@@ -16,29 +14,3 @@ module SayTests =
               testCase "Say hello all" <| fun _ ->
                   let subject = Say.hello "all"
                   Expect.equal subject "Hello all" "You didn't say hello" ]
-
-    let TS10Font = [
-        { width = 5; height = 0; depth = 12; italic = 0; larger = Some 16;
-            varChar = { top = None; bot = None; rep = None } };
-        { width = 4; height = 0; depth = 12; italic = 0; larger = None;
-            varChar = { top = Some 105; bot = Some 121; rep = Some 63 } }
-    ]
-
-
-    let fake_LoadFont (famliyName: string, fontSize: FontSize) =
-        // some work around
-        let fakeTestFontDir = System.AppContext.BaseDirectory +/ ".." +/ "fonts"
-        let fontDir =
-            match System.IO.Directory.Exists(fontDirPath) with
-            | true -> fontDirPath
-            | false -> fakeTestFontDir
-        getFontPath fontDir famliyName fontSize
-            |> (readFontLines fontSize)
-
-    [<Tests>]
-    let fonts =
-        testList "LoadFont test"
-            [ testCase "read font info" <| fun _ ->
-                let font = fake_LoadFont("TS", 10)
-                Expect.equal font TS10Font "Bad font!"
-            ]
---
+++ tests/TeXFormulaLayout.Tests/font/LoadFont.fs
@@ -0,0 +1,30 @@
+module TeXFormulaLayout.Tests.LoadFont
+open Expecto
+open TeXFormulaLayout.FontTypes
+open TeXFormulaLayout.LoadFont
+
+
+let TS10Font = [
+    { width = 5; height = 0; depth = 12; italic = 0; larger = Some 16;
+        varChar = { top = None; bot = None; rep = None } };
+    { width = 4; height = 0; depth = 12; italic = 0; larger = None;
+        varChar = { top = Some 105; bot = Some 121; rep = Some 63 } }
+]
+
+let fake_LoadFont (famliyName: string, fontSize: FontSize) =
+    // some work around
+    let fakeTestFontDir = System.AppContext.BaseDirectory +/ ".." +/ "fonts"
+    let fontDir =
+        match System.IO.Directory.Exists(fontDirPath) with
+        | true -> fontDirPath
+        | false -> fakeTestFontDir
+    getFontPath fontDir famliyName fontSize
+        |> (readFontLines fontSize)
+
+
+[<Tests>]
+let fonts = testList "LoadFont test" [
+    testCase "read font info" <| fun _ ->
+        let font = fake_LoadFont("TS", 10)
+        Expect.equal font TS10Font "Bad font!"
+]
