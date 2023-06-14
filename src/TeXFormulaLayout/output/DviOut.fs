bdaff15d32eca42688c770c08f924416d8721dfe
woclass <git@wo-class.cn>
2023-06-14 22:04:06 +0800

    Update `tailFill`

--- src/TeXFormulaLayout/output/DviOut.fs
+++ src/TeXFormulaLayout/output/DviOut.fs
@@ -319,4 +319,4 @@ module DviOut =
         trailer 3
 
     /// At least out one 233, Make final ownPos is a multiple of four bytes
-    let rec tailFill ownPos = trailer (4 - ownPos % 4)
+    let tailFill ownPos = trailer (4 - ownPos % 4)
